96185f5b8c06ee1f83f66b9a7c5618ffb42b6dc7
Kaushik Lakhani <kaushik.lakhani123@gmail.com>
2022-06-25 21:01:34 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const header= document.querySelector("header");
+
+
+/* -----------------Sticky Navbars--------------------------- */
+ function stickyNavbar(){
+     header.classList.toggle("Scrolled",window.pageYOffset > 0);
+ }
+
+
+window.addEventListener("scroll",stickyNavbar);
+
+
+var select = document.getElementById('category');
+
+document.getElementById("button").onclick = function(){
+    var value = select.value
+    if (value == "Handloom")
+    {
+        window.location.href = "Handloom.html";
+    }
+    else if (value == "Handicraft")
+    {
+        window.location.href = "Handicrafts.html";
+    }
+};
